7dcbe50d93487a2ea58ca98729c3f3640cccd077
Thiago Lira <thi.lira01@hotmail.com>
2023-06-21 23:39:01 -0300

    :tada initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')}
+  //if(html.classList.contains('light')) {
+    //html.classList.remove('light')
+ // } else {
+   // html.classList.add()
+  //}
+//}
+//essa acima é a logica da programação, comandos para criar listas e classes dentro do js 
+
